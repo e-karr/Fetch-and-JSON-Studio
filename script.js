88d5a0726b62cbf9738bc42843b342171a090d5c
Elizabeth Karr <e.karr93@icloud.com>
2023-04-18 13:53:53 -0500

    build fetch request

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // TODO: add code here
 window.addEventListener('load', () => {
-
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json){
+            console.log(json);
+        });
+    });
 });
