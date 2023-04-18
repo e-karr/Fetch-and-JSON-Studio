f08b9a0f13214b26c95f9325f024f8367432588b
Elizabeth Karr <e.karr93@icloud.com>
2023-04-18 14:45:24 -0500

    add space between each skill

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ window.addEventListener('load', () => {
                 return b.hoursInSpace - a.hoursInSpace;
             });
             json.forEach(element => {
+                let skills = element.skills.join(`, `);
                 container.innerHTML += `
                     <div class=astronaut>
                         <div class=bio>
@@ -14,18 +15,18 @@ window.addEventListener('load', () => {
                             <ul>
                                 <li>Hours in space: ${element.hoursInSpace}</li>
                                 <li>Active: ${element.active}</li>
-                                <li>Skills: ${element.skills}</li>
+                                <li>Skills: ${skills}</li>
                             </ul>
                         </div>
                         <img class=avatar src=${element.picture}>
                     </div>
                 `;
 
-                if (element.active) {
-                    console.log(element.active);
-                    // let active = document.querySelector(".bio>ul>li:nth-child(2)");
-                    // active.style.color = "green";
-                }
+                // if (element.active) {
+                //     console.log(element.active);
+                //     // let active = document.querySelector(".bio>ul>li:nth-child(2)");
+                //     // active.style.color = "green";
+                // }
             });
 
             container.innerHTML += `
