84060069a22f2abeb54bc893f4c837a6a4a09635
Elizabeth Karr <e.karr93@icloud.com>
2023-04-18 14:09:40 -0500

    loop through json array and dynamically add to html

--- script.js
+++ script.js
@@ -2,7 +2,22 @@
 window.addEventListener('load', () => {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then(function(json){
-            console.log(json);
+            let container = document.getElementById('container');
+            json.forEach(element => {
+                container.innerHTML += `
+                    <div class=astronaut>
+                        <div class=bio>
+                            <h3>${element.firstName} ${element.lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${element.hoursInSpace}</li>
+                                <li>Active: ${element.active}</li>
+                                <li>Skills: ${element.skills}</li>
+                            </ul>
+                        </div>
+                        <img class=avatar src=${element.picture}>
+                    </div>
+                `;
+            })
         });
     });
 });
